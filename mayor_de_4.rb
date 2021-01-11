8d430494422e6449cad645cafeb65cbd6315de77
Roguez84 <t.roguez984@gmail.com>
2021-01-10 21:14:55 -0300

    added mayor de 4 desafio

---
+++ mayor_de_4.rb
@@ -0,0 +1,19 @@
+uno = ARGV[0].to_i
+dos = ARGV[1].to_i
+tres = ARGV[2].to_i
+cuatro = ARGV[3].to_i
+
+puts [uno,dos,tres,cuatro].max()
+
+
+=begin
+if uno > dos && uno > tres && uno > cuatro
+    puts uno
+elsif dos > uno && dos > tres && dos > cuatro
+    puts dos
+elsif tres > uno && tres > dos && tres > cuatro
+    puts tres
+else
+    puts cuatro
+end
+=end
